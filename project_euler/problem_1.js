8b85adfe659c50f30f6164c9fdb087b678ce2a93
Ifeanyi Charles Munonye <charlesmuno@yahoo.com>
2016-07-31 18:11:31 -0500

    Create problem_1.js

---
+++ project_euler/problem_1.js
@@ -0,0 +1,26 @@
+// js solution
+var sum = 0;
+for (i=0; i<1000; i++){
+	if(i % 3 === 0 && i% 5 === 0){
+    sum = sum + i;
+	}
+	else if (i % 3 === 0 || i % 5 ===0){
+	  sum = sum + i;
+	
+	}
+};
+console.log(sum);
+
+
+
+
+//ruby solution
+sum = 0
+
+(1...1000).each do |i|
+     sum = sum + i if (i % 3 == 0 && i % 5 == 0) || i % 3 == 0 || i % 5 == 0 
+end
+
+puts sum
+
+
