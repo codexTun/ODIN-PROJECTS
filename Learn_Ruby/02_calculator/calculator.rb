3c13c94438ea7db0ba943a25d178502322664c77
Ifeanyi Charles Munonye <charlesmuno@yahoo.com>
2016-08-12 17:36:59 -0500

    <commit message>

---
+++ Learn_Ruby/02_calculator/calculator.rb
@@ -0,0 +1,34 @@
+#write your code here
+#rspec calculator_spec.rb
+def add (num1, num2)
+	num1 + num2
+end
+
+def subtract (num1, num2)
+	num1 - num2
+end
+
+def sum (array)
+    sum_array = 0
+	array.each do |x| sum_array += x
+	end
+	sum_array
+end
+
+def multiply (array)
+	sum_array = 1
+	array.each do |i| sum_array *= i
+	end
+	sum_array
+end
+
+def power (num1, num2)
+	num1**num2
+end
+
+def factorial(n)
+  (2..n).reduce(1, :*)
+end
+
+
+
