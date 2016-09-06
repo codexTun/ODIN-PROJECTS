697b6b212533a516593d7d5a018ddb1f31257450
IFEANYI MUNONYE <charlesmuno@yahoo.com>
2016-09-06 01:22:03 -0500

    form-based workflow feature completed

--- Ruby_projects/blogger/config/routes.rb
+++ Ruby_projects/blogger/config/routes.rb
@@ -1,4 +1,5 @@
 Blogger::Application.routes.draw do
-  resources :articles
+	root to:'articles#index'
+  	resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
