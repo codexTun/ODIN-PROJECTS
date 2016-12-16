c96950eea757d5653d10d25beea7fa1c895145ca
IFEANYI MUNONYE <charlesmuno@yahoo.com>
2016-12-16 09:12:47 -0600

    fibonacci file merge

--- Ruby_projects/Recursion/fibs_elegance.rb
+++ Ruby_projects/Recursion/fibs_elegance.rb
@@ -1,6 +1,18 @@
 #takes a number and returns that many members of the fibonacci sequence
 
-def fibo (n)
+def fibs_rec(n, i = 0,  ary = [0,1])
+  return ary if n == 2
+  
+  ary << ary[i] + ary[i+1]
+
+  i += 1
+  fibs_rec(n-1,i,ary )
+  
+end 
+
+#Nonrecursion 
+
+def fibs(n)
   arr = [0,1,1]
   a = 0
   b = 1
@@ -14,5 +26,4 @@ def fibo (n)
   end
   arr
 end
-  
-  
+
